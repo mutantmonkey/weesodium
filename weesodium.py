a8ab1d4633e4b45bea4a476f0390be34e5ea074c
mutantmonkey <mutantmonkey@mutantmonkey.in>
2014-10-28 19:29:36 -0700

    some cleanup, remove hardcoded keys

--- weesodium.py
+++ weesodium.py
@@ -2,23 +2,24 @@
 
 import base64
 import libnacl.secret
+import shlex
 import weechat
 
 SCRIPT_NAME = 'weesodium'
 SCRIPT_AUTHOR = 'mutantmonkey'
-SCRIPT_VERSION = '20141027'
+SCRIPT_VERSION = '20141028'
 SCRIPT_LICENSE = 'GPL3'
 SCRIPT_DESC = "encrypt messages in a channel with libsodium"
-#SCRIPT_COMMAND = 'weesodium'
+
+channel_keys = {}
+
 
 def encrypt(msg, key, max_length=None):
     # math.ceil(384 / 3) * 4 = 512
 
     if max_length is not None:
-        # FIXME: calculate how much space we actually need
-        # these are probably not correct
         pad_length = max_length - libnacl.crypto_secretbox_NONCEBYTES \
-                     - libnacl.crypto_secretbox_MACBYTES
+            - libnacl.crypto_secretbox_MACBYTES
         if len(msg) < pad_length:
             msg += b'\x00' * (pad_length - len(msg))
 
@@ -67,54 +68,91 @@ def parse_privmsg(message):
         raise Exception("Failed parsing PRIVMSG")
 
 
-def in_privmsg(data, modifier, modifier_data, string):
+def in_privmsg_cb(data, modifier, modifier_data, string):
     result = parse_privmsg(string)
+    if result['to_channel'] is not None:
+        dict_key = '{0}.{1}'.format(modifier_data, result['to_channel'])
+        if dict_key in channel_keys:
+            key = channel_keys[dict_key]
 
-    # TODO: decrypt message only if it is enabled for the channel and we have the key
-    # FIXME: take a key from weechat settings for this channel
-    key = b'\xf8\xe2\xdb\x94`\xdb7\xd1I08\xcf\xe0O \xf8\xb0\xcd\xc2\xd6\xf4\x0e\x9f\x8f&aps\x82\xd9\xf1\xd5'
-
-    try:
-        result['text'] = decrypt(result['text'], key)
-    except:
-        result['text'] = "Unable to decrypt: {}".format(result['text'])
-
-    # FIXME: sanitize these bits
-    return b":{from} PRIVMSG {to} :{text}".format(**result)
-
-
-def out_privmsg(data, modifier, modifier_data, string):
-    result = parse_privmsg(string)
-
-    # TODO: encrypt message only if it is enabled for the channel and we have the key
-    # FIXME: take a key from weechat settings for this channel
-    # maybe use weesodium.[plugin].[server].[channel] or local buffer variable?
-    key = b'\xf8\xe2\xdb\x94`\xdb7\xd1I08\xcf\xe0O \xf8\xb0\xcd\xc2\xd6\xf4\x0e\x9f\x8f&aps\x82\xd9\xf1\xd5'
-
-    max_length = 270
-    if len(result['text']) > max_length:
-        # segment messages larger than max_length
-        out = b""
-        splits = 1 + (len(result['text']) // max_length)
-        for i in range(0, splits):
-            text = encrypt(
-                result['text'][i * max_length:(i + 1) * max_length],
-                key,
-                max_length)
+            try:
+                result['text'] = decrypt(result['text'], key)
+            except:
+                result['text'] = "Unable to decrypt: {}".format(result['text'])
+                return string
 
             # FIXME: sanitize these bits
-            out += b"PRIVMSG {to} :{text}\n".format(to=result['to'], text=text)
+            return b":{from} PRIVMSG {to} :{text}".format(**result)
 
-        return out
+    return string
+
+
+def out_privmsg_cb(data, modifier, modifier_data, string):
+    result = parse_privmsg(string)
+    if result['to_channel'] is not None:
+        dict_key = '{0}.{1}'.format(modifier_data, result['to_channel'])
+        if dict_key in channel_keys:
+            key = channel_keys[dict_key]
+
+            max_length = 270
+            if len(result['text']) > max_length:
+                # segment messages larger than max_length
+                out = b""
+                splits = 1 + (len(result['text']) // max_length)
+                for i in range(0, splits):
+                    text = encrypt(
+                        result['text'][i * max_length:(i + 1) * max_length],
+                        key,
+                        max_length)
+
+                    # FIXME: sanitize these bits
+                    out += b"PRIVMSG {to} :{text}\n".format(to=result['to'], text=text)
+
+                return out
+            else:
+                result['text'] = encrypt(result['text'], key, max_length)
+
+                # FIXME: sanitize these bits
+                return b"PRIVMSG {to} :{text}".format(**result)
+
+    return string
+
+
+def command_cb(data, buf, args):
+    try:
+        args = shlex.split(args)
+    except:
+        return weechat.WEECHAT_RC_ERROR
+
+    if len(args) == 2 and args[0] == b'enable':
+        server, channel = get_buffer_info(buf)
+        channel_keys['{0}.{1}'.format(server, channel)] = args[1]
+        return weechat.WEECHAT_RC_OK
+    elif len(args) == 1 and args[0] == b'disable':
+        server, channel = get_buffer_info(buf)
+        del channel_keys['{0}.{1}'.format(server, channel)]
+        return weechat.WEECHAT_RC_OK
     else:
-        result['text'] = encrypt(result['text'], key, max_length)
+        return weechat.WEECHAT_RC_ERROR
 
-        # FIXME: sanitize these bits
-        return b"PRIVMSG {to} :{text}".format(**result)
+
+def get_buffer_info(buf):
+    server = weechat.buffer_get_string(buf, b'localvar_server').decode('utf-8')
+    channel = weechat.buffer_get_string(buf, b'localvar_channel').decode(
+        'utf-8')
+    return server, channel
 
 
 if weechat.register(SCRIPT_NAME, SCRIPT_AUTHOR, SCRIPT_VERSION, SCRIPT_LICENSE,
                     SCRIPT_DESC, "", "UTF-8"):
     # consider adding: notice, topic
-    weechat.hook_modifier('irc_in_privmsg', 'in_privmsg', '')
-    weechat.hook_modifier('irc_out_privmsg', 'out_privmsg', '')
+    weechat.hook_modifier('irc_in_privmsg', 'in_privmsg_cb', '')
+    weechat.hook_modifier('irc_out_privmsg', 'out_privmsg_cb', '')
+    weechat.hook_command(SCRIPT_NAME, "change weesodium options",
+    "[enable KEY] || "
+    "[disable]",
+    "",
+    "enable %-|| "
+    "disable",
+    "command_cb",
+    "")
